06c49f075fa9d9351af04883cabdc26ad21a6d80
Vesper Gray Wetzel <deborah.wetzel@mykenzie.snhu.edu>
2023-04-01 14:46:58 -0600

    Working on Landing Page

--- server/src/config/server.config.js
+++ server/src/config/server.config.js
@@ -1,3 +1,3 @@
 export const port = process.env.PORT || 3001;
-export const db_url = process.env.DB_URL || "mongodb://localhost/capstone";
+export const db_url = process.env.DB_URL || "mongodb+srv://KENZIEVESPER:PASSWORD@cluster0.vak6nro.mongodb.net/?retryWrites=true&w=majority";
 export const jwt_secret = process.env.JWT_SECRET || "enterpasswordhere";
